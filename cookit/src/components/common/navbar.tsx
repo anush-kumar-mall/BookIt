5182d92dfb7f64e4bdf142577d8d32463c020e0c
Anush Kumar Mall <mallanushkumar@gmail.com>
2025-10-29 15:22:12 +0530

    created reaact app

---
+++ cookit/src/components/common/navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      {/* Your navbar code here */}
+    </div>
+  );
+}
+
+// 👇 Add this line if you still get the error
+export {};
---
+++ cookit/src/pages/mainPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/common/navbar";
+
+export default function MainPage() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+// 👇 Add this line if you still get the error
+export {};
